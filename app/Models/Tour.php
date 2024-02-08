6c47b81e37c4d02620b6199bf991a9a6319e4f8c
Emre Dikmen <emredikmen002@gmail.com>
2024-02-08 10:51:32 +0300

    create tour model and migration

---
+++ app/Models/Tour.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+
+class Tour extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'travel_id',
+        'name',
+        'starting_date',
+        'ending_date',
+        'price',
+    ];
+
+    public function travel(): BelongsTo
+    {
+        return $this->belongsTo(Travel::class);
+    }
+}
--- app/Models/Travel.php
+++ app/Models/Travel.php
@@ -6,8 +6,8 @@ use Cviebrock\EloquentSluggable\Sluggable;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletes;
-
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Travel extends Model
 {
@@ -23,6 +23,10 @@ class Travel extends Model
         'number_of_days',
     ];
 
+    protected $casts = [
+        'is_admin' => 'boolean'
+    ];
+
     public function sluggable(): array
     {
         return [
@@ -30,5 +34,17 @@ class Travel extends Model
                 'source' => 'name'
             ]
         ];
+    }
+
+    public function tours(): HasMany
+    {
+        return $this->hasMany(Tour::class);
+    }
+
+    public function numberOfNights(): Attribute
+    {
+        return Attribute::make(
+            get: fn($value, $attribute) => $attribute['number_of_days'] - 1
+        );
     }
 }
