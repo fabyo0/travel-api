15576adaf8106bac660d7db80a7ea49462e0e85c
Emre Dikmen <emredikmen002@gmail.com>
2024-02-08 10:16:02 +0300

    models and migrations for the travel role have been created. The role_user pivot table has been added to manage the relationship

---
+++ app/Models/Travel.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+
+class Travel extends Model
+{
+    use HasFactory;
+
+    protected $table = 'travels';
+
+    protected $fillable = [
+        'is_public',
+        'slug',
+        'name',
+        'description',
+        'number_of_days',
+    ];
+}
