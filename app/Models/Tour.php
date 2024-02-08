06c90cad30302da9aa9aec075145c10e2e94f210
Emre Dikmen <emredikmen002@gmail.com>
2024-02-08 13:18:28 +0300

    tour price attribute

--- app/Models/Tour.php
+++ app/Models/Tour.php
@@ -2,10 +2,10 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Database\Eloquent\SoftDeletes;
 
 
 class Tour extends Model
@@ -23,5 +23,14 @@ class Tour extends Model
     public function travel(): BelongsTo
     {
         return $this->belongsTo(Travel::class);
+    }
+
+    // when returned to Frontends, they will be formatted (99900 / 100);
+    public function price(): Attribute
+    {
+        return Attribute::make(
+            get: fn($value) => $value / 100,
+            set: fn($value) => $value * 100,
+        );
     }
 }
